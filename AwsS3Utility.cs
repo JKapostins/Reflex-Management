e8cf8935d30915b257c5450de0581e2d0e2780bb
jkapostins@gnarlysoft.net <JKapostins@gnarlysoft.net>
2018-12-31 01:56:59 -0500

    Adding library of code that is shared between aws lambdas and the reflex track management tool

---
+++ AwsS3Utility.cs
@@ -0,0 +1,20 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TrackManagement
+{
+    public class AwsS3Utility
+    {
+        public static async Task UploadFileAsync(Stream fileStream, string bucketName, string keyName, RegionEndpoint bucketRegion)
+        {
+            using (var s3Client = new AmazonS3Client(bucketRegion))
+            {
+                var fileTransferUtility = new TransferUtility(s3Client);
+                await fileTransferUtility.UploadAsync(fileStream, bucketName, keyName);
+            }
+        }
+    }
+}
---
+++ Track.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TrackManagement
+{
+    public class TrackType
+    {
+        public const string National = "National";
+        public const string Supercross = "Supercross";
+        public const string FreeRide = "FreeRide";
+        public const string Unknown = "Unknown";
+    }
+
+    public class Track
+    {
+        public Track()
+        {
+            TrackType = TrackManagement.TrackType.Unknown;
+            TrackName = string.Empty;
+            TrackUrl = string.Empty;
+            ThumbnailUrl = string.Empty;
+            SourceTrackUrl = string.Empty;
+            SourceThumbnailUrl = string.Empty;
+            Author = string.Empty;
+            SlotNumber = 0;
+            ErrorInfo = string.Empty;
+            Valid = true;
+            Rating = 0.0f;
+            RatingVoteCount = 0;
+        }
+
+        public string TrackType { get; set; }
+        public string TrackName { get; set; }
+        public string TrackUrl { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string SourceTrackUrl { get; set; }
+        public string SourceThumbnailUrl { get; set; }        
+        public string Author { get; set; }
+        public string ErrorInfo { get; set; }
+        public long CreationTime { get; set; }
+        public float Rating { get; set; }
+        public int RatingVoteCount { get; set; }
+        public int SlotNumber { get; set; }
+        public bool Valid { get; set; }
+
+        //DynamoDB doesn't allow null or empty string values... 
+        public void FixEmptyStrings()
+        {
+            TrackType = TrackType == string.Empty ? "null" : TrackType;
+            TrackName = TrackName == string.Empty ? "null" : TrackName;
+            TrackUrl = TrackUrl == string.Empty ? "null" : TrackUrl;
+            ThumbnailUrl = ThumbnailUrl == string.Empty ? "null" : ThumbnailUrl;
+            SourceTrackUrl = SourceTrackUrl == string.Empty ? "null" : SourceTrackUrl;
+            SourceThumbnailUrl = SourceThumbnailUrl == string.Empty ? "null" : SourceThumbnailUrl;
+            Author = Author == string.Empty ? "null" : Author;
+            ErrorInfo = ErrorInfo == string.Empty ? "null" : ErrorInfo;
+        }
+    }
+}
