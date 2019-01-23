ff48b16a574e83adc97adab0a70c70326e2468d1
jkapostins@gnarlysoft.net <JKapostins@gnarlysoft.net>
2019-01-23 14:01:22 -0500

    Adding callstack to the exception logger.

--- ExceptionLogger.cs
+++ ExceptionLogger.cs
@@ -7,6 +7,10 @@ namespace ReflexUtility
         public static void LogException(Exception e)
         {
             Console.Error.WriteLine(e.Message);
+            Console.Error.WriteLine("Call Stack:");
+            Console.Error.WriteLine(e.StackTrace);
+            Console.WriteLine("");
+
             if (e.InnerException != null)
             {
                 LogException(e.InnerException);
